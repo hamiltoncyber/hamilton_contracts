39a981ddb74371d2d8a58cae04a71d2d5c1c7efa
smtechceo <40452378+smtechceo@users.noreply.github.com>
2022-07-18 16:47:18 +0800

    init IERC726 ERC726
    Initialize files ERC726.sol and ERC726.sol

---
+++ contracts/token/ERC726/IERC726.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// SmarTech Contracts (last updated v1.0.0) (token/ERC726/IERC726.sol)
+
+pragma solidity ^0.8.0;
+
+import "../ERC721/IERC721.sol";
+
+/**
+ * @dev Required interface of an ERC721 compliant contract.
+ */
+interface IERC726 is IERC721 {
+
+
+}
